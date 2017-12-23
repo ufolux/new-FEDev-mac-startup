5bba71b41dba04d5eb8f732784317d5d6d149346
Sines <sines@Siness-MBP-2.lan>
2017-12-23 22:54:57 +0800

    add iterm2

--- mac-startup.sh
+++ mac-startup.sh
@@ -89,6 +89,9 @@ colorTitle "step 14.install other tools"
 colorSubTitle " -install node-canvas"
 brew install pkg-config cairo pango libpng jpeg giflib
 
+colorSubTitle " -install iterm2"
+brew cask install iterm2
+
 colorSubTitle " -install shadowsocksx"
 brew cask install shadowsocksx
 
@@ -115,6 +118,9 @@ brew cask install bilibili
 
 colorSubTitle " -install Aria2GUI"
 brew cask install aria2gui
+
+colorSubTitle " -install VSCode"
+brew cask install visual-studio-code
 
 colorTitle "step 15.install adnroid sdk tools with sdkmanager"
 sdkmanager "add-ons;addon-google_apis-google-15" "add-ons;addon-google_apis-google-16" "add-ons;addon-google_apis-google-17" "add-ons;addon-google_apis-google-18" "add-ons;addon-google_apis-google-19" "add-ons;addon-google_apis-google-21" "add-ons;addon-google_apis-google-22" "add-ons;addon-google_apis-google-23" "add-ons;addon-google_apis-google-24" "build-tools;19.1.0" "build-tools;20.0.0" "build-tools;21.1.2" "build-tools;22.0.1" "build-tools;23.0.1" "build-tools;23.0.2" "build-tools;23.0.3" "build-tools;24.0.0" "build-tools;24.0.1" "build-tools;24.0.2" "build-tools;24.0.3" "build-tools;25.0.0" "build-tools;25.0.1" "build-tools;25.0.2" "build-tools;25.0.3" "build-tools;26.0.0" "build-tools;26.0.1" "build-tools;26.0.2" "cmake;3.6.4111459" "extras;android;m2repository" "extras;google;m2repository" "extras;google;webdriver" "lldb;2.0" "lldb;2.1" "lldb;2.2" "lldb;2.3" "ndk-bundle" "patcher;v4" "platform-tools" "platforms;android-15" "platforms;android-16" "platforms;android-17" "platforms;android-18" "platforms;android-19" "platforms;android-20" "platforms;android-21" "platforms;android-22" "platforms;android-23" "platforms;android-24" "platforms;android-25" "platforms;android-26" "tools"
